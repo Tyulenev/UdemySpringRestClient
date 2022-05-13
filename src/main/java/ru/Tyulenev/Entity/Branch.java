b79ea04da7e4db2dceb8ead973e0dc37cc5c8b51
Alexey Tyulenev <alexey.tyulenev@q-systems.ru>
2022-05-13 17:11:32 +0300

    Added Branch Entity

---
+++ src/main/java/ru/Tyulenev/Entity/Branch.java
@@ -0,0 +1,25 @@
+package ru.Tyulenev.Entity;
+
+public class Branch {
+    private int id;
+    private String name;
+
+    public Branch() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
